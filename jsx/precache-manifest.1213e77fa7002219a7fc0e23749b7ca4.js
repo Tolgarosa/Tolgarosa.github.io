548333a396d0d6afdeff9258c878256702b23813
Tolga <tolgabilsel@gmail.com>
2019-01-28 16:12:18 +0100

    jsx add

---
+++ jsx/precache-manifest.1213e77fa7002219a7fc0e23749b7ca4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aefa9a50f363cece9f3",
+    "url": "/jsx/static/js/main.8aefa9a5.chunk.js"
+  },
+  {
+    "revision": "794557b9fe620e78fc3d",
+    "url": "/jsx/static/js/1.794557b9.chunk.js"
+  },
+  {
+    "revision": "31bcc97bda4e250142fe",
+    "url": "/jsx/static/js/runtime~main.31bcc97b.js"
+  },
+  {
+    "revision": "d78566c4e09471c1c96088b303561d26",
+    "url": "/jsx/index.html"
+  }
+];
